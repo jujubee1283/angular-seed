703106d9ac36b4e4b2df737a7f94768e01197966
ju7915 <ju7915@att.com>
2015-02-11 15:01:54 -0800

    update karma config and dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -11,6 +11,15 @@ module.exports = function(config){
       'app/view*/**/*.js'
     ],
 
+    // list of files / patterns to exclude
+    exclude: [],
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    reporters: ['spec'],
+
     autoWatch : true,
 
     frameworks: ['jasmine'],
@@ -21,7 +30,9 @@ module.exports = function(config){
             'karma-chrome-launcher',
             'karma-firefox-launcher',
             'karma-jasmine',
-            'karma-junit-reporter'
+            'karma-junit-reporter',
+            'karma-phantomjs-launcher',
+            'karma-spec-reporter'
             ],
 
     junitReporter : {
